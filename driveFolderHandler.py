4a26bf86a7a08abd3a5eadd3b39f756809dbf6da
phil-cork-epic <pcork@policyinnovation.org>
2023-12-15 13:18:52 -0500

    Further modularize Handler function for future use cases

--- driveFolderHandler.py
+++ driveFolderHandler.py
@@ -1,20 +1,20 @@
 import gdown
-def downloadTempDriveFolder(folderID):
+def downloadTempDriveFolder(folderID, path):
     '''
     Takes the folder ID of a publicly accessible Drive folder and 
-    downloads the files within to /temp-docs one folder up from the current directory.
+    downloads the files within to /temp-docs to the provided path.
     Drive folder ID is found by copying the sharing link and removing everything 
     except the string of characters between 'https://drive.google.com/drive/folders/' and '?usp=drive_link' 
     '''
     gdown.download_folder(id= folderID, 
-                          output= "../temp-docs", 
+                          output= path, 
                           quiet= False, 
                           use_cookies= False)
-    print("Files stored in /temp-docs")
+    print("Files stored in " + path)
     
 
 import shutil
-def deleteTempDriveFolder():
+def deleteTempDriveFolder(path):
     '''Deletes the folder and files created by downloadTempDriveFolder()'''
-    shutil.rmtree('../temp-docs')
-    print("Deleted folder and all files within /temp-docs")
+    shutil.rmtree(path)
+    print("Deleted folder and all files within " + path)

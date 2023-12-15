63bfe38ec1529790fa77ea9b7892e929d228e62a
phil-cork-epic <pcork@policyinnovation.org>
2023-12-15 13:19:10 -0500

    Standardize RI for data dictionary review

--- year1/RI/code/clean-RI.R
+++ year1/RI/code/clean-RI.R
@@ -4,37 +4,38 @@ library(janitor)
 
 clean_ri <- function() {
   
+  # (129, 16)
   ri_comp <- fread("year1/RI/data/39-RhodeIsland_PPL_Comprehensive.csv",
                    colClasses = "character", na.strings = "") %>% 
-    clean_names() %>%
-    # drop columns
-    select(-a, -b, -c, -d, -e, -f, -g, -source_fund, -new, -est_start_date) %>%
-    # process numeric columns
+    clean_names()
+  
+  # -> (129, 10)
+  ri_clean <- ri_comp %>%
+    
     mutate(
-      funding_amount = as.numeric(str_replace_all(funds_requested,"[^0-9.]", "")),
+      requested_amount = as.numeric(str_replace_all(funds_requested,"[^0-9.]", "")),
       population = as.numeric(str_replace_all(pop_served,"[^0-9.]", "")),
       
     ) %>%
-    # process text columns
     mutate(borrower = str_squish(system_name),
            borrower = str_remove(borrower, "\\*"),
            state_score = str_replace_all(scores_total,"[^0-9.]", ""),
            pwsid = paste0("RI", pws_id),
            project_description = str_squish(project_description),
+           project_type = case_when(
+             grepl("PFAS", project_description, ignore.case=TRUE) ~ "Emerging Contaminants",
+             grepl("Lead", project_description, ignore.case=TRUE) ~ "Lead",
+             TRUE ~ "General"),
            state = "Rhode Island",
-           category = ""
-    )
+           category = "2",
+           funding_status = "Not Funded"
+    ) %>%
+    
+    select(borrower, pwsid, project_type, requested_amount, project_description, population,
+           state_score, funding_status, state, category)
   
-  ri_fund <- fread("year1/RI/data/39-RhodeIsland_PPL_Fundable_Manual.csv",
-                   colClasses = "character", na.strings = "") %>% 
-    clean_names() %>%
-    mutate(funding_amount = as.numeric(str_replace_all(loan_amount,"[^0-9.]", "")),
-           funding_status = "Funded",
-           borrower = str_remove(borrower, "\\*")) %>%
-    select(-loan_amount) %>%
-    left_join(ri_comp, by="borrower")
   
   rm(list=setdiff(ls(), "ri_clean"))
   
-  return(NULL)
+  return(ri_clean)
 }

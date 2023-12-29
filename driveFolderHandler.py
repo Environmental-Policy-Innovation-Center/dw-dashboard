d827f9181c28618d42a1802618f64128ed30d8ac
phil-cork-epic <pcork@policyinnovation.org>
2023-12-29 13:58:55 -0600

    Scrape ME Applicant Data

--- driveFolderHandler.py
+++ driveFolderHandler.py
@@ -2,7 +2,7 @@ import gdown
 def downloadTempDriveFolder(folderID, path):
     '''
     Takes the folder ID of a publicly accessible Drive folder and 
-    downloads the files within to /temp-docs to the provided path.
+    downloads the files within to the provided path.
     Drive folder ID is found by copying the sharing link and removing everything 
     except the string of characters between 'https://drive.google.com/drive/folders/' and '?usp=drive_link' 
     '''

e73b33668dcdaf1ad24393b34ac6cee401a2d639
phil-cork-epic <pcork@policyinnovation.org>
2024-02-01 14:40:36 -0500

    Update FL applicant data

--- year1/FL/code/clean-FL.R
+++ year1/FL/code/clean-FL.R
@@ -4,7 +4,7 @@ library(janitor)
 
 clean_fl <- function() {
 
-  fl_fund <- fread("year1/FL/data/9-Florida_ppl_fundable.csv",
+  fl_clean <- fread("year1/FL/data/9-Florida_ppl_fundable.csv",
                    colClasses = "character", na.strings = "") %>%
     clean_names() %>%
     # format numeric columns
@@ -16,31 +16,19 @@ clean_fl <- function() {
            project_name = str_squish(project_number),
            project_description = str_squish(project_description),
            state_score = str_replace_all(priority_score,"[^0-9.]",""),
-           funding_status = "Funded") %>%
-    select(borrower, project_name, project_description, state_score, funding_amount, 
-           principal_forgiveness_amount, funding_status)
-  
-  #NOTE: This file was created manually as it only includes two rows
-  fl_waiting <- fread("year1/FL/data/9-Florida_ppl_WaitingList.csv",
-                      colClasses = "character", na.strings = "") %>%
-    clean_names() %>%
-    # format number columns
-    mutate(project_cost = as.numeric(str_replace_all(estimated_unfunded_cost,"[^0-9.]",""))) %>%
-    # format text columns 
-    mutate(borrower = str_squish(applicant),
-           project_name = str_squish(project_nbr),
-           project_description = str_squish(project_description),
-           state_score = str_squish(priority_score),
-           funding_status = "Not Funded") %>%
-    select(borrower, project_name, project_description, state_score, project_cost, funding_status)
-    
-    
-  fl_clean <- bind_rows(fl_fund, fl_waiting) %>%
-    mutate(state = "Flordia",
+           funding_status = case_when(
+             project_name == "0604D" ~ "Not Funded",
+             project_name == "53200" & project_description == "Treatment (DIW Pump Station & RO Effluent Removal) Distribution" ~ "Not Funded",
+             TRUE ~ "Funded"),
            project_type = "General",
-           category = "1")
-  
-  # rm(list=setdiff(ls(), "fl_clean"))
+           category = "1",
+           state = "Florida"
+           ) %>%
+    select(borrower, project_name, project_description, state_score, funding_amount, 
+           principal_forgiveness_amount, funding_status, project_type, category, state)
 
-  return(NULL)
+  
+  rm(list=setdiff(ls(), "fl_clean"))
+
+  return(fl_clean)
 }

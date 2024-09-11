2a799cfd5092c70f9418d478b522de3583fff676
phil-cork-epic <pcork@policyinnovation.org>
2024-09-11 14:02:25 -0400

    Add qa-qc notes for IL Y2

--- year2/IL/code/clean-IL.R
+++ year2/IL/code/clean-IL.R
@@ -1,20 +1,15 @@
-# Set the working directory
-setwd("/Users/madhvimalhotra/Downloads/EPIC/Github/dw-dashboard")
+# this should be handled by opening the Project file so that all of the working directory references are the same across users
 
-# Load required libraries
-library(tidyverse)
-library(data.table)
-library(janitor)
+# these libraries are pulled in by resources.R automatically
+
 
 source("resources.R")
 
 clean_il <- function() {
   base_path <- file.path("year2", "IL", "data")
   
-  #Clean and convert numeric values
-  clean_numeric <- function(x) {
-    as.character(as.numeric(gsub("[^0-9.-]", "", x)))
-  }
+  # this function reproduces `clean_numeric_string` in resources.R
+
   
   # Read PPL Fundable data
   il_ppl_f <- fread(file.path(base_path, "y2-Illinois_PPL_Fundable.csv"),
@@ -22,8 +17,9 @@ clean_il <- function() {
     clean_names() %>%
     mutate(expecting_funding = "Yes",
            project_type = "General",
-           disadvantaged_community_principal_forgiveness = str_replace(disadvantaged_community_principal_forgiveness, "N/E", "0"),
-           requested_loan_amount = clean_numeric(requested_loan_amount))
+           # when previewing the data, there isn't an instance of "N/E" in the data, so this line shouldn't be needed?
+           # please correct if I've missed something.
+           requested_amount = clean_numeric_string(requested_loan_amount))
   
   # Read PPL Applicant data
   il_ppl_a <- fread(file.path(base_path, "y2-Illinois_PPL_Applicant.csv"),
@@ -31,13 +27,16 @@ clean_il <- function() {
     clean_names() %>%
     mutate(expecting_funding = "No",
            project_type="General",
-           requested_loan_amount = clean_numeric(requested_loan_amount))
+           requested_amount = clean_numeric_string(requested_loan_amount))
   
   # Combine PPL data
   il_ppl <- bind_rows(il_ppl_a, il_ppl_f) %>%
+    # rows where disadvantaged_community_principal_forgiveness are NA should be No Information per the data dictionary
+    # whereas 0 should be coded as No
     mutate(disadvantaged = case_when(
-      as.numeric(gsub("[^0-9.-]", "", disadvantaged_community_principal_forgiveness)) > 0 ~ "Yes",
-      TRUE ~ "No"
+      is.na(disadvantaged_community_principal_forgiveness) ~ "No Information",
+      disadvantaged_community_principal_forgiveness == "0" ~ "No",
+      TRUE ~ "Yes"
     ))
   
   # Create list of communities and DAC status
@@ -55,11 +54,14 @@ clean_il <- function() {
   il_lead <- merge(il_lead, dacs, by = "loan_applicant", all.x = TRUE) %>%
     mutate(
       disadvantaged = coalesce(disadvantaged, "No Information"),
-      reserved_principal_forgiveness_amount = clean_numeric(reserved_principal_forgiveness_amount),
-      reserved_loan_amount = clean_numeric(reserved_loan_amount),
-      funding_amount = as.character(as.numeric(reserved_principal_forgiveness_amount) + as.numeric(reserved_loan_amount)),
-      principal_forgiveness = reserved_principal_forgiveness_amount,
-      requested_loan_amount = reserved_loan_amount
+      # this can just be stored as principal_forgiveness and reduce the code
+      principal_forgiveness = clean_numeric_string(reserved_principal_forgiveness_amount),
+      # this can just be stored as requested_amount and reduce the code
+      requested_amount = clean_numeric_string(reserved_loan_amount),
+      #NOTE: this works because these features don't have any NAs
+      # in other states where they are incomplete, use the convert_to_numeric() function
+      # fill in the 0s, conduct the necessary math, and then replace any artificially created 0s with "No Information"
+      funding_amount = as.character(as.numeric(principal_forgiveness) + as.numeric(requested_amount)),
     )
   
   # Process Emerging Contaminant data
@@ -72,19 +74,22 @@ clean_il <- function() {
   il_ec <- merge(il_ec_f, dacs, by = "loan_applicant", all.x = TRUE) %>%
     mutate(
       disadvantaged = coalesce(disadvantaged, "No Information"),
-      requested_loan_amount = clean_numeric(requested_loan_amount),
-      principal_forgiveness_reserved = clean_numeric(principal_forgiveness_reserved),
-      funding_amount = as.character(as.numeric(requested_loan_amount) + as.numeric(principal_forgiveness_reserved)),
-      principal_forgiveness = principal_forgiveness_reserved
+      # this can just be stored as requested_amount and reduce the code
+      requested_amount = clean_numeric_string(requested_loan_amount),
+      principal_forgiveness = clean_numeric_string(principal_forgiveness_reserved),
+      funding_amount = as.character(as.numeric(requested_amount) + as.numeric(principal_forgiveness)),
     )
   
   # Combine all data
   il_merge <- bind_rows(il_ppl, il_lead, il_ec) %>%
     mutate(
-      funding_amount = if_else(project_type == "General", requested_loan_amount, funding_amount),
-      disadvantaged_community_principal_forgiveness = clean_numeric(disadvantaged_community_principal_forgiveness),
-      service_population = clean_numeric(service_population),
-      principal_forgiveness = coalesce(clean_numeric(principal_forgiveness), "0")
+      funding_amount = if_else(project_type == "General", requested_amount, funding_amount),
+      # don't need the disadvantaged pf column after this, so can remove
+      # can just assign population here rather than renaming downstream
+      population = clean_numeric_string(service_population),
+      # we don't fill NAs with 0, but use 'No Information' - a state reporting 0 for funding or PF is different 
+      # than not providing any info at all for our purposes
+      principal_forgiveness = coalesce(clean_numeric_string(principal_forgiveness), "No Information")
     )
   
   # Clean and process data
@@ -92,29 +97,26 @@ clean_il <- function() {
     mutate(
       borrower = str_squish(loan_applicant),
       borrower = str_to_title(borrower, locale="en"),
+      # this modification of project_score is fine, but unnecessary
       project_score = str_replace_all(loan_priority_score, "[^0-9.]", ""),
       project_description = str_squish(project_description),
       project_description = str_to_sentence(project_description),
       state = "Illinois",
       state_fiscal_year = "2024",
       community_served = as.character(NA),
-      project_id = coalesce(l17_number, ""),
+      project_id = coalesce(l17_number, "No Information"),
       project_name = as.character(NA),
       project_cost = as.character(NA),
-      requested_amount = requested_loan_amount,
       project_rank = as.character(NA),
-      population = service_population,
-      pwsid = coalesce(facility_no, ""),
-      principal_forgiveness = coalesce(principal_forgiveness, "0")  # Ensure no NA values
+      pwsid = coalesce(facility_no, "No Information"),
+      # principal_forgiveness is already complete, don't need to re-coalesce
     ) %>%
     select(community_served, borrower, pwsid, project_id, project_name, project_type, project_cost,
            requested_amount, funding_amount, principal_forgiveness, population, project_description,
            disadvantaged, project_rank, project_score, expecting_funding, state, state_fiscal_year)
   
-  # Replace NA values
-  columns_to_replace_na <- c("community_served", "project_name", "project_cost", "project_rank", "project_score", "principal_forgiveness")
-  il_clean <- il_clean %>%
-    mutate(across(all_of(columns_to_replace_na), ~coalesce(., "")))
+  # Columns that are completely missing SHOULD be NA. Any NAs in columns where we have incomplete information
+  # should be filled in with "No Information"
   
   # Verify final shape of il_clean
   cat("Final shape of il_clean:", dim(il_clean), "\n")

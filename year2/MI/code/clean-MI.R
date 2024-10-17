c8095ff3c9d8a55b53b9059be227be3a1282b55b
phil-cork-epic <pcork@policyinnovation.org>
2024-10-17 17:37:06 -0400

    Update MI Y2

--- year2/MI/code/clean-MI.R
+++ year2/MI/code/clean-MI.R
@@ -1,59 +1,58 @@
-library(tidyverse)
-library(data.table)
-library(janitor)
+source("resources.R")
 
 clean_mi <- function() {
-  
-  ## Work in progress as a result of working matching Michigan projects to Districts for Funding Navigator team
-  #TODO: update funding_status and confirm all other columns upon data dictionary being made and reviewed
-  
- 
+
   mi_raw <- fread("year2/MI/data/mi-sfy24-iup.csv",
-                  colClasses = "character", na.strings = "")
+                  colClasses = "character", na.strings = "") %>%
+    clean_names()
 
   mi_clean <- mi_raw %>%
-    ## clean names
-    clean_names() %>%
     ## get rid of rows without projects
     filter(!is.na(project_number)) %>%
-    ## rowwise operations for sums
-    rowwise() %>%
-    ## make relevant columns numbers
-    mutate(
-           population = as.numeric(gsub(",", "", population)),
-           ## sum of columns, getting rid of $ and comma and replacing blanks with zero
-           funding_amount = sum(as.numeric(gsub("\\$|,", "", dwsrf_traditional_loan)),
-                                as.numeric(gsub("\\$|,", "", dwsrf_traditional_pf)),
-                                as.numeric(gsub("\\$|,", "", bil_dwsrf_supplemental_loan)),
-                                as.numeric(gsub("\\$|,", "", bil_dwsrf_supplemental_pf)),
-                                as.numeric(gsub("\\$|,", "", bil_dwsrf_emerging_contaminants_pf)),
-                                as.numeric(gsub("\\$|,", "", bil_dwsrf_lslr_loan)),
-                                as.numeric(gsub("\\$|,", "", bil_dwsrf_lslr_pf)),
-                                na.rm = T),
-           principal_forgiveness_amount = sum(as.numeric(str_replace_all(dwsrf_traditional_pf,"[^0-9.]", "")),
-                                              as.numeric(str_replace_all(bil_dwsrf_supplemental_pf,"[^0-9.]", "")),
-                                              as.numeric(str_replace_all(bil_dwsrf_emerging_contaminants_pf,"[^0-9.]", "")),
-                                              as.numeric(str_replace_all(bil_dwsrf_lslr_pf,"[^0-9.]", "")),
-                                              na.rm = T)) %>%
-    ungroup() %>%
-    ## standardize names
-    rename(borrower = applicant,
-           cities_served = county) %>%
-    ## create project type column
-    mutate(project_type = case_when(lead_service_line_costs != "" ~ "Lead",
-                                    ec_related_costs != "" ~ "Emerging Contaminants"),
-           project_type = replace_na(project_type, "General"),
-           # with state_rank used for specifying funding, return to string
-           state_score = str_squish(total_points),
-           disadvantaged = replace_na(disadvantaged_status, "No"),
+           #convert all of the various funding columns to numeric and add for funding and PF
+    mutate(dwsrf_traditional_loan = convert_to_numeric(dwsrf_traditional_loan, TRUE),
+           dwsrf_traditional_pf = convert_to_numeric(dwsrf_traditional_pf, TRUE),
+           bil_dwsrf_supplemental_loan = convert_to_numeric(bil_dwsrf_supplemental_loan, TRUE),
+           bil_dwsrf_supplemental_pf = convert_to_numeric(bil_dwsrf_supplemental_pf, TRUE),
+           bil_dwsrf_emerging_contaminants_pf = convert_to_numeric(bil_dwsrf_emerging_contaminants_pf, TRUE),
+           bil_dwsrf_lslr_loan = convert_to_numeric(bil_dwsrf_lslr_loan, TRUE),
+           bil_dwsrf_lslr_pf = convert_to_numeric(bil_dwsrf_lslr_pf, TRUE),
+           
+           funding_amount = dwsrf_traditional_loan + dwsrf_traditional_pf + bil_dwsrf_supplemental_loan + bil_dwsrf_supplemental_pf +
+             bil_dwsrf_emerging_contaminants_pf + bil_dwsrf_lslr_loan + bil_dwsrf_lslr_pf,
+           funding_amount = clean_numeric_string(funding_amount),
+           
+           principal_forgiveness = dwsrf_traditional_pf + bil_dwsrf_supplemental_pf + bil_dwsrf_emerging_contaminants_pf + bil_dwsrf_lslr_pf,
+           principal_forgiveness = clean_numeric_string(principal_forgiveness),
+           
+           project_id = str_squish(project_number),
+           project_cost = clean_numeric_string(total_project_costs),
+           project_description = str_squish(project_components),
+           population = clean_numeric_string(population),
+           borrower = str_squish(applicant),
+           community_served = str_squish(county),
+           project_type = case_when(lead_service_line_costs != "" ~ "Lead",
+                                    ec_related_costs != "" ~ "Emerging Contaminants",
+                                    TRUE ~ "General"),
+           project_score = str_squish(total_priority_points),
+           disadvantaged = ifelse(is.na(disadvantaged_status), "No", "Yes"),
            state = "Michigan",
-           category = "1",
+           state_fiscal_year = "2024",
+           pwsid = as.character(NA),
+           project_name = as.character(NA),
+           requested_amount = as.character(NA),
+           project_rank = as.character(NA),
+           expecting_funding = case_when(funding_amount != "0" ~ "Yes",
+           TRUE ~ "No")
+          
     ) %>%
     ## keep relevant columns
-    select(borrower, cities_served, project_description, project_type,
-           state_score, funding_amount, principal_forgiveness_amount,
-           disadvantaged, population, state, category)
+    select(community_served, borrower, pwsid, project_id, project_name, project_type, project_cost,
+           requested_amount, funding_amount, principal_forgiveness, population, project_description,
+           disadvantaged, project_rank, project_score, expecting_funding, state, state_fiscal_year)
   
+  
+  run_tests(mi_clean)
   rm(list=setdiff(ls(), "mi_clean"))
   
   return(mi_clean)

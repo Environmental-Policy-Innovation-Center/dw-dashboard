240234dd0d2ea13a4be284f116d6044357e4b083
EmmaLiTsai <emmali.tsai@gmail.com>
2025-03-20 08:38:51 -0400

    rescraped & standardized TN y1

--- year1/TN/code/clean-TN.R
+++ year1/TN/code/clean-TN.R
@@ -2,19 +2,26 @@ source("resources.R")
 
 clean_tn_y1 <- function() {
   
-  tn_ppl <- fread("year1/TN/data/tn-fy22-ppl.csv",
-                    colClasses = "character", na.strings = "") %>%
+  # (143, 19) - this file has been updated with the correct one on 
+  # March 19th 2025
+  tn_ppl <- fread("year1/TN/data/sfy_23_basegen_supp_ppl.csv",
+                  colClasses = "character", na.strings = "") %>%
     clean_names() 
   
   tn_clean <- tn_ppl %>%
-    # process numeric columns
+    # # process numeric columns
     mutate(project_cost = clean_numeric_string(total_project_amount),
            population = clean_numeric_string(pop_served)) %>%
     # process text columns 
     mutate(community_served = str_squish(county),
            borrower = str_squish(local_government),
            pwsid = str_squish(pwsid_number),
+           project_id = as.character(NA),
+           project_name = as.character(NA), 
            project_type = "General",
+           requested_amount = as.character(NA), 
+           funding_amount = as.character(NA),
+           principal_forgiveness = as.character(NA), 
            project_description = str_squish(project_description),
            disadvantaged = case_when(
              grepl("\\*", local_government) ~ "Yes",
@@ -24,17 +31,10 @@ clean_tn_y1 <- function() {
            borrower = str_replace_all(borrower, "\\+", ""),
            project_rank = str_squish(rank_order),
            project_score = str_squish(priority_points),
-           expecting_funding = "No",
+           expecting_funding = as.character(NA),
            state = "Tennessee",
-           state_fiscal_year = "2023",
-           project_id = as.character(NA),
-           project_name = as.character(NA),
-           requested_amount = as.character(NA),
-           funding_amount = as.character(NA),
-           principal_forgiveness = as.character(NA),
-           project_rank = replace_na(project_rank, "No Information"),
-           project_score = replace_na(project_score, "No Information")
-           ) %>%
+           state_fiscal_year = "2023"
+    ) %>%
     select(community_served, borrower, pwsid, project_id, project_name, project_type, project_cost,
            requested_amount, funding_amount, principal_forgiveness, population, project_description,
            disadvantaged, project_rank, project_score, expecting_funding, state, state_fiscal_year)

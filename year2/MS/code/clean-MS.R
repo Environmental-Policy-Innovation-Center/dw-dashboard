d75ffeddd4e90c89fddf0b793efac339f93a0b24
mariaEPIC <maria@policyinnovation.org>
2025-07-23 14:14:11 -0400

    Mississippi y1-y4 processing

--- year2/MS/code/clean-MS.R
+++ year2/MS/code/clean-MS.R
@@ -1,16 +1,52 @@
-clean_ms_y1 <- function() {
- 
+clean_ms_y2 <- function() {
+
+  curated_file_names <- stringr::str_subset(list.files("year2/MS/data"), "curated")
+  
   # read in data (extracted with tabula, further inspected and curated)
-  priority_list<- data.table::fread("year1/MS/data/PPL_MS_FFY2022_raw_curated.csv",
-                  colClasses = "character", na.strings = "") |> 
-    janitor::clean_names() |>
-    dplyr::mutate(source_list="Priority")
+  # there are many data fils, so this read in structure may be more efficient.
+  # However, the base funding file first must be read in first, because it has 
+  # a "funding line" element that provides special information about it's projects.
+
+  # list files
+  PPL_files <- stringr::str_subset(curated_file_names, "PPL")  
+  PPlaL_files <- stringr::str_subset(curated_file_names, "PPlaL")
+
+  # read file
+  priority_list <- dplyr::tibble()
+  for (file_name in PPL_files){
+    tmp_priority_list <- data.table::fread(paste0("year2/MS/data/", file_name), colClasses = "character", na.strings = "") |> 
+      janitor::clean_names() |>
+      dplyr::mutate(
+        source_list="Priority",
+        source_file = dplyr::case_when(
+          stringr::str_detect(file_name, "_LSLR") ~ "LSLR",
+          stringr::str_detect(file_name, "_EC") ~ "EC",
+          TRUE ~ "Base"
+        )
+      )
+    priority_list<- priority_list |>
+      dplyr::bind_rows(tmp_priority_list)
+  }
   
-  planning_list<- data.table::fread("year1/MS/data/PPlaL_MS_FFY2022_raw_curated.csv",
-                  colClasses = "character", na.strings = "") |> 
-    janitor::clean_names() |>
-    dplyr::mutate(source_list="Planning")  
   
+  planning_list <- dplyr::tibble()
+  for (file_name in PPlaL_files){
+    tmp_planning_list <- data.table::fread(paste0("year2/MS/data/", file_name), colClasses = "character", na.strings = "") |> 
+      janitor::clean_names() |>
+      dplyr::mutate(
+        source_list="Planning",
+        source_file = dplyr::case_when(
+          stringr::str_detect(file_name, "_LSLR") ~ "LSLR",
+          stringr::str_detect(file_name, "_EC") ~ "EC",
+          TRUE ~ "Base"
+        )
+      )
+    
+    planning_list<- planning_list |>
+      dplyr::bind_rows(tmp_planning_list)
+    
+  }
+
   # sanity check
   colnames(priority_list) == colnames(planning_list)
 
@@ -19,7 +55,6 @@ clean_ms_y1 <- function() {
     dplyr::bind_rows(planning_list) |>
     # drop category rows
     dplyr::filter(!grepl("Category", project)) |>
-    dplyr::mutate(dplyr::across(dplyr::everything(), ~ dplyr::na_if(.x, "#VALUE!"))) |>
     dplyr::mutate(
       geographic_reference = "No information", #this changed from community served?
       borrower = stringr::str_squish(project),
@@ -30,6 +65,8 @@ clean_ms_y1 <- function() {
     dplyr::mutate(
       project_description = stringr::str_squish(project_description),
       project_type = dplyr::case_when(
+            stringr::str_detect("LSLR", source_file) ~ "Lead",
+            stringr::str_detect("EC", source_file) ~ "Emerging Contaminants",
             stringr::str_detect(lead_str, project_description) ~ "Lead",
             stringr::str_detect(ec_str, project_description) ~ "Emerging Contaminants",
             TRUE ~ "General"
@@ -49,18 +86,14 @@ clean_ms_y1 <- function() {
           unlist(lapply(funding_lines, function(x) seq_len(x - 1)))
         },
         clean_numeric_string(eligible_pf_amount),
-        # checked in w Danielle about this on 2025_07_17, 
-        # we use NA instead of 0, because projects below the funding line 
-        # are not expecting funding and principal forgiveness would not apply 
-        # to those cases.
-        "NA") 
+        "No Information") 
       ) |>
     dplyr::mutate(
       population = clean_numeric_string(service_area_population),
       disadvantaged = ifelse(
              as.numeric(str_replace_all(eligible_pf_amount,"[^0-9.]","")) > 0, 
              "Yes", 
-             "No"), 
+             "No"), # No information available for SFY24 EC IUP-2 
       project_score = str_replace_all(priority_points,"[^0-9.]",""),
       # determine expecting funding based on funding line (regardless of amount)
       expecting_funding = dplyr::if_else(
@@ -69,15 +102,16 @@ clean_ms_y1 <- function() {
           unlist(lapply(funding_lines, function(x) seq_len(x - 1)))
         },
         "Yes",
-        "No") 
+        "No")  #No information: No information for projects on the SFY24 LSLR IUP-1, SFY24 LSLR IUP-2, SFY24 EC IUP-1, or SFY24 EC IUP-2
+
     )|>
     dplyr::filter(!stringr::str_detect("Funding Line--", project)) |>
     dplyr::group_by(source_list) |>
-    dplyr::mutate(project_rank = paste0(dplyr::row_number(), "-",source_list)) |>
+    dplyr::mutate(project_rank = paste0(dplyr::row_number(), "-", source_file)) |>
     dplyr::ungroup() |>
     dplyr::mutate(
       state = "Mississippi",
-      state_fiscal_year = "2023"
+      state_fiscal_year = "2024"
     ) |>
     dplyr::select(geographic_reference, borrower, pwsid, project_id, project_name, project_type, project_cost,
            requested_amount, funding_amount, principal_forgiveness, population, project_description,
---
+++ year3/MS/code/clean-MS.R
@@ -0,0 +1,113 @@
+clean_ms_y3 <- function() {
+
+  curated_file_names <- stringr::str_subset(list.files("year3/MS/data"), "curated")
+  
+  # read in data (extracted with tabula, further inspected and curated)
+  # there are many data fils, so this read in structure may be more efficient.
+  # However, the base funding file first must be read in first, because it has 
+  # a "funding line" element that provides special information about it's projects.
+
+  # list files
+  PPL_files <- stringr::str_subset(curated_file_names, "PPL")  
+  PPlaL_files <- stringr::str_subset(curated_file_names, "PPlaL")
+
+  # read file
+  priority_list <- dplyr::tibble()
+  for (file_name in PPL_files){
+    tmp_priority_list <- data.table::fread(paste0("year3/MS/data/", file_name), colClasses = "character", na.strings = "") |> 
+      janitor::clean_names() |>
+      dplyr::mutate(
+        source_list="Priority",
+        source_file = dplyr::case_when(
+          stringr::str_detect(file_name, "_LSLR") ~ "LSLR",
+          stringr::str_detect(file_name, "_EC") ~ "EC",
+          TRUE ~ "Base"
+        )
+      )
+    priority_list<- priority_list |>
+      dplyr::bind_rows(tmp_priority_list)
+  }
+  
+  
+  planning_list <- dplyr::tibble()
+  for (file_name in PPlaL_files){
+    tmp_planning_list <- data.table::fread(paste0("year3/MS/data/", file_name), colClasses = "character", na.strings = "") |> 
+      janitor::clean_names() |>
+      dplyr::mutate(
+        source_list="Planning",
+        source_file = dplyr::case_when(
+          stringr::str_detect(file_name, "_LSLR") ~ "LSLR",
+          stringr::str_detect(file_name, "_EC") ~ "EC",
+          TRUE ~ "Base"
+        )
+      )
+    
+    planning_list<- planning_list |>
+      dplyr::bind_rows(tmp_planning_list)
+    
+  }
+
+  # sanity check
+  colnames(priority_list) == colnames(planning_list)
+
+  # order matters, priority list should be read in first
+  ms_clean <- priority_list |> 
+    dplyr::bind_rows(planning_list) |>
+    # drop category rows
+    dplyr::filter(!grepl("Category", project)) |>
+    dplyr::mutate(
+      geographic_reference = "No information", #this changed from community served?
+      borrower = stringr::str_squish(project),
+      pwsid = "No information",
+      project_id = "No information",
+      project_name = "No information"
+    ) |>
+    dplyr::mutate(
+      project_description = stringr::str_squish(project_title),
+      project_type = dplyr::case_when(
+            stringr::str_detect("LSLR", source_file) ~ "Lead",
+            stringr::str_detect("EC", source_file) ~ "Emerging Contaminants",
+            stringr::str_detect(lead_str, project_title) ~ "Lead",
+            stringr::str_detect(ec_str, project_title) ~ "Emerging Contaminants",
+            TRUE ~ "General"
+           )
+    ) |>
+    dplyr::mutate(
+      project_cost = "No information",
+      requested_amount = clean_numeric_string(loan_amount_requested),
+      funding_amount = "No Information"
+    )|> 
+    dplyr::mutate(
+      row_id = dplyr::row_number(),
+      principal_forgiveness = "No Information") |>
+    dplyr::mutate(
+      population = clean_numeric_string(service_area_population),
+      disadvantaged = ifelse(
+             as.numeric(str_replace_all(eligible_pf_amount,"[^0-9.]","")) > 0, 
+             "Yes", 
+             "No"),
+      project_score = str_replace_all(priority_points,"[^0-9.]","")
+    ) |>
+    dplyr::mutate(  
+      expecting_funding = dplyr::if_else(
+        source_list == "Planning",
+        "No",
+        "No Information"))|>
+    dplyr::group_by(source_list) |>
+    dplyr::mutate(project_rank = paste0(dplyr::row_number(), "-", source_list)) |>
+    dplyr::ungroup() |>
+    dplyr::mutate(
+      state = "Mississippi",
+      state_fiscal_year = "2025"
+    ) |>
+    dplyr::select(geographic_reference, borrower, pwsid, project_id, project_name, project_type, project_cost,
+           requested_amount, funding_amount, principal_forgiveness, population, project_description,
+           disadvantaged, 
+           project_rank, 
+           project_score, expecting_funding, state, state_fiscal_year)
+  
+  run_tests(ms_clean)
+  rm(list=setdiff(ls(), "ms_clean"))
+  
+  return(ms_clean)
+}

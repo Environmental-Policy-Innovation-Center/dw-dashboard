0fbec90c48f15e893ae392e0113bb74541d0bc27
mariaEPIC <maria@policyinnovation.org>
2025-07-30 12:44:30 -0400

    MI data processing Y0-Y3

--- year2/MI/code/clean-MI.R
+++ year2/MI/code/clean-MI.R
@@ -4,51 +4,69 @@ clean_mi_y2 <- function() {
                   colClasses = "character", na.strings = "") %>%
     clean_names()
 
-  mi_clean <- mi_raw %>%
-           #convert all of the various funding columns to numeric and add for funding and PF
-    mutate(dwsrf_traditional_loan = convert_to_numeric(dwsrf_traditional_loan, TRUE),
-           dwsrf_traditional_pf = convert_to_numeric(dwsrf_traditional_pf, TRUE),
-           bil_dwsrf_supplemental_loan = convert_to_numeric(bil_dwsrf_supplemental_loan, TRUE),
-           bil_dwsrf_supplemental_pf = convert_to_numeric(bil_dwsrf_supplemental_pf, TRUE),
-           bil_dwsrf_emerging_contaminants_pf = convert_to_numeric(bil_dwsrf_emerging_contaminants_pf, TRUE),
-           bil_dwsrf_lslr_loan = convert_to_numeric(bil_dwsrf_lslr_loan, TRUE),
-           bil_dwsrf_lslr_pf = convert_to_numeric(bil_dwsrf_lslr_pf, TRUE),
-           
-           funding_amount = dwsrf_traditional_loan + dwsrf_traditional_pf + bil_dwsrf_supplemental_loan + bil_dwsrf_supplemental_pf +
-             bil_dwsrf_emerging_contaminants_pf + bil_dwsrf_lslr_loan + bil_dwsrf_lslr_pf,
-           funding_amount = clean_numeric_string(funding_amount),
-           
-           principal_forgiveness = dwsrf_traditional_pf + bil_dwsrf_supplemental_pf + bil_dwsrf_emerging_contaminants_pf + bil_dwsrf_lslr_pf,
-           principal_forgiveness = clean_numeric_string(principal_forgiveness),
-           
-           project_id = str_squish(project_number),
-           project_id = replace_na(project_id, "No Information"),
-           project_cost = clean_numeric_string(total_project_costs),
-           project_description = str_squish(project_components),
-           population = clean_numeric_string(population),
-           borrower = str_squish(applicant),
-           community_served = str_squish(county),
-           project_type = case_when(grepl("LSL", project_components) ~ "Lead",
-                                    bil_dwsrf_emerging_contaminants_pf > 0 ~ "Emerging Contaminants",
-                                    TRUE ~ "General"),
-           project_score = str_squish(total_priority_points),
-           disadvantaged = ifelse(is.na(disadvantaged_status), "No", "Yes"),
-           state = "Michigan",
-           state_fiscal_year = "2024",
-           pwsid = as.character(NA),
-           project_name = as.character(NA),
-           requested_amount = as.character(NA),
-           project_rank = as.character(NA),
-           expecting_funding = case_when(funding_amount != "0" ~ "Yes",
-           TRUE ~ "No")
-          
-    ) %>%
+  mi_clean <- mi_raw |>
+    dplyr::mutate(
+      community_served = stringr::str_squish(county),
+      borrower = stringr::str_squish(applicant),
+      pwsid = as.character(NA),
+      project_id = dplyr::case_when(
+        is.na(project_number) ~ "No Information",
+        .default = stringr::str_squish(project_number)
+      ),
+      project_name = as.character(NA),
+      project_type =  case_when(
+             grepl(lead_str, project_components, ignore.case=TRUE) | !is.na(lead_service_line_costs)  ~ "Lead",
+             grepl(ec_str, project_components, ignore.case=TRUE) | !is.na(ec_related_costs) ~ "Emerging Contaminants",
+             TRUE ~ "General"),
+      project_cost = clean_numeric_string(total_project_costs),
+      requested_amount = as.character(NA),
+
+      funding_amount = convert_to_numeric(dwsrf_traditional_loan, TRUE) + 
+        convert_to_numeric(dwsrf_traditional_pf, TRUE) +
+        convert_to_numeric(bil_dwsrf_supplemental_loan, TRUE) +
+        convert_to_numeric(bil_dwsrf_supplemental_pf, TRUE) +
+        convert_to_numeric(bil_dwsrf_emerging_contaminants_pf, TRUE) +
+        convert_to_numeric(bil_dwsrf_lslr_loan, TRUE) +
+        convert_to_numeric(bil_dwsrf_lslr_pf, TRUE),
+      funding_amount = clean_numeric_string(funding_amount), 
+      principal_forgiveness = convert_to_numeric(dwsrf_traditional_pf, TRUE) +
+        convert_to_numeric(bil_dwsrf_supplemental_pf, TRUE) +
+        convert_to_numeric(bil_dwsrf_emerging_contaminants_pf, TRUE) +
+        convert_to_numeric(bil_dwsrf_lslr_pf, TRUE),
+      principal_forgiveness = clean_numeric_string(principal_forgiveness),
+      population = stringr::str_squish(population),
+      project_description = stringr::str_squish(project_components),
+      disadvantaged = dplyr::case_when(
+        is.na(disadvantaged_status) ~ "No",
+        .default = "Yes"
+      ),
+      project_rank = as.character(NA),
+      project_score = stringr::str_squish(total_priority_points),
+      expecting_funding = dplyr::case_when(funding_amount != "0" ~ "Yes",
+           TRUE ~ "No"),
+      state = "Michigan",
+      state_fiscal_year = "2024"
+    ) |>   
     ## keep relevant columns
     select(community_served, borrower, pwsid, project_id, project_name, project_type, project_cost,
            requested_amount, funding_amount, principal_forgiveness, population, project_description,
            disadvantaged, project_rank, project_score, expecting_funding, state, state_fiscal_year)
   
+  ####### SANITY CHECKS START #######
   
+  # Hone in on project id duplication
+  
+  mi_clean |> dplyr::group_by(project_id) |> dplyr::summarise(counts = n()) |> dplyr::arrange(dplyr::desc(counts))
+  # mi_clean |> dplyr::filter(project_id == "7588-01")
+  ####### Decision: Keep as two separate projects, one gets funding from Gen and the other from Lead
+ 
+  
+  # Check for disinfection byproduct in description
+  mi_clean |> dplyr::filter(grepl("disinfection byproduct", tolower(project_description)))
+  ####### Decision: No disinfection byproduct string
+    
+  ####### SANITY CHECKS END #######
+
   run_tests(mi_clean)
   rm(list=setdiff(ls(), "mi_clean"))
   
--- year3/MI/code/clean-MI.R
+++ year3/MI/code/clean-MI.R
@@ -4,51 +4,66 @@ clean_mi_y3 <- function() {
                   colClasses = "character", na.strings = "") %>%
     clean_names()
   
-  mi_clean <- mi_raw %>%
-    filter(!is.na(project_number)) %>%
-    #convert all of the various funding columns to numeric and add for funding and PF
-    mutate(dwsrf_loan = convert_to_numeric(dwsrf_loan, TRUE),
-           dwsrf_pf = convert_to_numeric(dwsrf_pf, TRUE),
-           bil_dwsrf_supplemental_loan = convert_to_numeric(bil_dwsrf_supplemental_loan, TRUE),
-           bil_dwsrf_supplemental_pf = convert_to_numeric(bil_dwsrf_supplemental_pf, TRUE),
-           bil_emerging_contaminant_pf = convert_to_numeric(bil_emerging_contaminant_pf, TRUE),
-           bil_dwsrf_lslr_loan = convert_to_numeric(bil_dwsrf_lslr_loan, TRUE),
-           bil_dwsrf_lslr_pf = convert_to_numeric(bil_dwsrf_lslr_pf, TRUE),
-           
-           funding_amount = dwsrf_loan + dwsrf_pf + bil_dwsrf_supplemental_loan + bil_dwsrf_supplemental_pf +
-             bil_emerging_contaminant_pf + bil_dwsrf_lslr_loan + bil_dwsrf_lslr_pf,
-           funding_amount = clean_numeric_string(funding_amount),
-           
-           principal_forgiveness = dwsrf_pf + bil_dwsrf_supplemental_pf + bil_emerging_contaminant_pf + bil_dwsrf_lslr_pf,
-           principal_forgiveness = clean_numeric_string(principal_forgiveness),
-           
-           requested_amount = clean_numeric_string(total_loan_amount_requested),
-           
-           project_id = str_squish(project_number),
-           project_description = str_squish(project_scope),
-           population = clean_numeric_string(population_served_by_project),
-           borrower = str_squish(applicant),
-           community_served = str_squish(project_county),
-           project_type = case_when(bil_emerging_contaminant_pf > 0 ~ "Emerging Contaminants",
-                                    grepl("LSLR", project_scope) ~ "Lead",
-                                    TRUE ~ "General"),
-           project_score = str_squish(total_priority_points),
-           disadvantaged = ifelse(is.na(overburdened_determination), "No", "Yes"),
-           state = "Michigan",
-           state_fiscal_year = "2025",
-           pwsid = as.character(NA),
-           project_name = as.character(NA),
-           project_rank = as.character(NA),
-           project_cost = as.character(NA),
-           expecting_funding = case_when(funding_amount != "0" ~ "Yes",
-                                         TRUE ~ "No")
-           
-    ) %>%
+  mi_clean <- mi_raw |>
+    filter(!is.na(project_number)) |>
+    dplyr::mutate(
+      community_served = stringr::str_squish(project_county),
+      borrower = stringr::str_squish(applicant),
+      pwsid = as.character(NA),
+      project_id = dplyr::case_when(
+        is.na(project_number) ~ "No Information",
+        .default = stringr::str_squish(project_number)
+      ),
+      project_name = as.character(NA),
+      project_type =  case_when(
+             grepl(lead_str, project_scope, ignore.case=TRUE) | !is.na(bil_lslr_eligible_costs)  ~ "Lead",
+             grepl(ec_str, project_scope, ignore.case=TRUE) | !is.na(emerging_contaminant_costs) ~ "Emerging Contaminants",
+             TRUE ~ "General"),
+      project_cost = as.character(NA),
+      requested_amount = clean_numeric_string(total_loan_amount_requested),
+      funding_amount = convert_to_numeric(dwsrf_loan, TRUE) + 
+        convert_to_numeric(dwsrf_pf, TRUE) +
+        convert_to_numeric(bil_emerging_contaminant_pf, TRUE) +
+        convert_to_numeric(bil_dwsrf_supplemental_loan, TRUE) +
+        convert_to_numeric(bil_dwsrf_supplemental_pf, TRUE) +
+        convert_to_numeric(bil_dwsrf_lslr_loan, TRUE) +
+        convert_to_numeric(bil_dwsrf_lslr_pf, TRUE),
+      funding_amount = clean_numeric_string(funding_amount), 
+      principal_forgiveness = convert_to_numeric(dwsrf_pf, TRUE) +
+        convert_to_numeric(bil_emerging_contaminant_pf, TRUE) +
+        convert_to_numeric(bil_dwsrf_supplemental_pf, TRUE) +
+        convert_to_numeric(bil_dwsrf_lslr_pf, TRUE),
+      principal_forgiveness = clean_numeric_string(principal_forgiveness),
+      population = as.character(NA),
+      project_description = stringr::str_squish(project_scope),
+      disadvantaged = dplyr::case_when(
+        is.na(overburdened_determination) ~ "No",
+        .default = "Yes"
+      ),
+      project_rank = as.character(NA),
+      project_score = stringr::str_squish(total_priority_points),
+      expecting_funding = dplyr::case_when(funding_amount != "0" ~ "Yes",
+           TRUE ~ "No"),
+      state = "Michigan",
+      state_fiscal_year = "2025"
+    ) |>
     ## keep relevant columns
     select(community_served, borrower, pwsid, project_id, project_name, project_type, project_cost,
            requested_amount, funding_amount, principal_forgiveness, population, project_description,
            disadvantaged, project_rank, project_score, expecting_funding, state, state_fiscal_year)
   
+  ####### SANITY CHECKS START #######
+  
+  # Hone in on project id duplication
+  
+  mi_clean |> dplyr::group_by(project_id) |> dplyr::summarise(counts = n()) |> dplyr::arrange(dplyr::desc(counts))
+  ####### Decision: No duplicates
+  
+  # Check for disinfection byproduct in description
+  mi_clean |> dplyr::filter(grepl("disinfection byproduct", tolower(project_description)))
+  ####### Decision: No disinfection byproduct string
+    
+  ####### SANITY CHECKS END #######
   
   run_tests(mi_clean)
   rm(list=setdiff(ls(), "mi_clean"))

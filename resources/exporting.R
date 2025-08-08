eac6315c1e10d74a8e9fdb8f878d0c7d724ba873
mariaEPIC <maria@policyinnovation.org>
2025-08-08 13:11:02 -0400

    add MS viz google sheet

--- resources/exporting.R
+++ resources/exporting.R
@@ -83,7 +83,8 @@ get_gs_urls <- function() {
     add_row(state="OH", url="https://docs.google.com/spreadsheets/d/1NE0Pl4p1Yv_lXBSIoLS89KpqmIUBWtBPoLQzj2GH2F4") %>%
     add_row(state="PA", url="https://docs.google.com/spreadsheets/d/1-MrH5VRuPABvovqz9fHBGufc-Zz2vpEqZcrPgQyNQzI") %>%
     add_row(state="TN", url="https://docs.google.com/spreadsheets/d/1mCDIMe41m4vJDnekpigRfB_sEvN74Z646J5a4VTvKKU") %>%
-    add_row(state="LA", url="https://docs.google.com/spreadsheets/d/17jeMqu1AznH8ns52A5ZbdYQou39u7PLCC_s2DS5i44c")
+    add_row(state="LA", url="https://docs.google.com/spreadsheets/d/17jeMqu1AznH8ns52A5ZbdYQou39u7PLCC_s2DS5i44c") |>
+    add_row(state="MS", url="https://docs.google.com/spreadsheets/d/1ajQLY_iwzIUW4vVV6JQKrayWml2fYSn0BBnB4As9Dzk")
   
   return(gs_urls)
 }

d9a4090a51f145caef935077c38d2027865a87c0
mariaEPIC <maria@policyinnovation.org>
2025-09-18 09:45:18 -0400

    data for Tennessee y3 and cleaning script

--- year3/TN/code/clean-TN.R
+++ year3/TN/code/clean-TN.R
@@ -1,46 +1,22 @@
 clean_tn_y3 <- function() {
   
-  # there were a few new additions to the EC string on June 12th 2025 to include
-  # various new strings 
-  ec_str_tn <- "cyanotoxin|dioxane|emerging contaminant|lithium|manganese|Mn|Perfluoro-n-pentanoic acid|PFPeA|PFAS|PFOA|PFOS|trihalomethane|THM|Unregulated Contaminant Monitoring Rule|DBP|disinfection byproduct|HAA5|haloacetic acid"
-  
-  # (64, 21) - gen PPL
-  gen_ppl <- fread("year3/TN/data/basegen_supp_ppl.csv",
-                   colClasses = "character", na.strings = "") %>%
-    clean_names() %>%
-    # manually remove projects found on lead/ec list already
-    filter(is.na(on_lead_ec)) %>%
-    mutate(project_type = case_when(grepl(lead_str, project_description, ignore.case = TRUE) ~ "Lead", 
-                                    grepl(ec_str, project_description, ignore.case = TRUE) ~ "Emerging Contaminants",
-                                    TRUE ~ "General"),
+  comprehensive_ppl <- fread("year3/TN/data/comprehensive_ppl.csv",
+                   colClasses = "character", na.strings = "") |>
+    clean_names() |>
+    mutate(
+           project_description = str_squish(project_description), 
+           project_type = dplyr::case_when(
+             grepl(lead_str, project_description, ignore.case=TRUE) ~ "Lead",
+             grepl(ec_str, project_description, ignore.case=TRUE) ~ "Emerging Contaminants",
+             TRUE ~ "General"),
            # these rows are blank for projects below the ranking line & not 
            # expecting funding
            project_rank = replace_na(rank_order, "No Information"), 
            project_score = replace_na(priority_points, "No Information"),
            expecting_funding = case_when(project_rank == "No Information" ~ "No", 
                                          TRUE ~ "Yes"))
-
-  # (9, 14) - emerging contaminants 
-  ec_ppl <- fread("year3/TN/data/ec_ppl.csv",
-                   colClasses = "character", na.strings = "") %>%
-    clean_names() %>%
-    mutate(project_type = "Emerging Contaminants")
   
-  # (7, 14) lead projects: 
-  lead_ppl <- fread("year3/TN/data/lead_ppl.csv",
-                    colClasses = "character", na.strings = "") %>%
-    clean_names() %>%
-    mutate(project_type = "Lead")
-  
-  # (16, 18) - binding and fixing the rank order for lead and ec projects: 
-  lead_ec <- bind_rows(ec_ppl, lead_ppl) %>%
-    # adding extra columns that are the same for both ppls: 
-    mutate(project_rank = as.character(rank_order), 
-           expecting_funding = "Yes", 
-           project_score = priority_points)
-  
-  # (80, 18) cleaning - binding gen, lead & EC together 
-  tn_clean <- bind_rows(gen_ppl, lead_ec) %>%
+  tn_clean <- comprehensive_ppl %>%
     mutate(community_served = str_squish(county),
            # removing weird characters from local government column
            borrower = str_replace_all(local_government, "\\*", ""),
@@ -63,7 +39,17 @@ clean_tn_y3 <- function() {
            requested_amount, funding_amount, principal_forgiveness, population, project_description,
            disadvantaged, project_rank, project_score, expecting_funding, state, state_fiscal_year)
   
+  ####### SANITY CHECKS START #######
   
+  # Hone in on project id duplication
+  ####### Decision: No project ids to check
+  
+  # Check for disinfection byproduct in description
+  tn_clean |> dplyr::filter(grepl("disinfection byproduct", tolower(project_description)))
+  ####### Decision: No change, classified as expected
+    
+  ####### SANITY CHECKS END #######
+
   run_tests(tn_clean)
   rm(list=setdiff(ls(), "tn_clean"))
   

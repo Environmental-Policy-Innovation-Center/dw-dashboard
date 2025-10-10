acd6ee8f1e5be4090d2f4fe5452fa6b0cd149fa5
EmmaLiTsai <emmali.tsai@gmail.com>
2025-10-10 07:40:31 -0400

    re-standardized oh y1

--- year1/OH/code/clean-OH.R
+++ year1/OH/code/clean-OH.R
@@ -1,41 +1,56 @@
 clean_oh_y1 <- function() {
   
-  ## Base, p25-35
-  # -> (440,9)
-  oh_base <- fread("year1/OH/data/oh-ppl-base.csv",
-                   colClasses = "character", na.strings = "") %>%
-    clean_names() %>%
-    select(-v1) %>%
+  # ohio EC string from data dictionary 
+  oh_ec_str <- "cyanotoxin|dioxane|emerging contaminant|lithium|manganese|Mn|Perfluoro-n-pentanoic acid|PFPeA|PFAS|PFOA|PFOS|trihalomethanes|THM|Unregulated Contaminant Monitoring Rule|DBP|HAA5|haloacetic acid"
+  
+  ## Base fundable list 
+  oh_fundable <- fread("year1/OH/data/oh-ppl-base.csv",
+                   colClasses = "character", na.strings = "") |>
+    clean_names() |>
+    select(-v1) |>
     # reset all column names and drop column name row
     rename(entity = v2, project = v3, pwsid = v4, population = v5, county = v6, 
-           estimated_loan_amount = v7, loan_type = v8, estimated_award_date = v9, rate = v10) %>%
-    filter(entity != "Entity")
+           estimated_loan_amount = v7, loan_type = v8, estimated_award_date = v9, rate = v10) |>
+    filter(entity != "Entity") |>
+    mutate(funding_amount = clean_numeric_string(estimated_loan_amount),
+           project = str_squish(project),
+           project_type = case_when(grepl(oh_ec_str, project) | grepl("HAB|PFAS", rate) ~ "Emerging Contaminants", 
+                                    # this will be overwritten for lead projects
+                                    # when we merge these three tables
+                                    TRUE ~ "General"),
+           # the "Mn" EC string incorrectly captured this as a EC project
+           project_type = case_when(project == "Watermain Imps Bun. 1 - Grange Hall Booster Station Wtr Mns" ~ "General",
+                                    TRUE ~ project_type),
+           expecting_funding = "Yes")
   
-  
-  ## PF
-  oh_pf <- fread("year1/OH/data/oh-ppl-pf.csv",
-                 colClasses = "character", na.strings = "") %>%
-    clean_names() %>%
-    filter(!is.na(estimated_principal_forgiveness)) %>%
-    select(entity, estimated_loan_amount, estimated_principal_forgiveness)
-    
+  ## DAC PF
+  oh_dac_ppl <- fread("year1/OH/data/oh-ppl-pf.csv",
+                      colClasses = "character", na.strings = "") |>
+    clean_names()
   
   ## Regional PF
-  #
-  oh_reg_pf <- fread("year1/OH/data/oh-ppl-regional-pf.csv",
-                 colClasses = "character", na.strings = "") %>%
-    clean_names() %>%
-    filter(!is.na(estimated_principal_forgiveness)) %>%
-    select(entity, estimated_loan_amount, estimated_principal_forgiveness)
+  # TODO - figure out if these are also disadvantaged 
+  oh_reg_ppl <- fread("year1/OH/data/oh-ppl-regional-pf.csv",
+                      colClasses = "character", na.strings = "") |>
+    clean_names() 
   
-  oh_pf <- bind_rows(oh_pf, oh_reg_pf) %>%
-    filter(estimated_principal_forgiveness != "BYPASS")
-  
+  # binding principal forgiveness tables: 
+  oh_pf <- bind_rows(oh_dac_ppl, oh_reg_ppl) |>
+    filter(estimated_principal_forgiveness != "BYPASS") |>
+    mutate(project_score = str_squish(project_score), 
+           # TODO - check with funding tracker team the EC tag should apply to
+           # both the dac and regionalization ppls: 
+           project_type = case_when(grepl("HAB|PFAS", rate) ~ "Emerging Contaminants", 
+                                    # this will be overwritten for lead projects
+                                    # when we merge these three tables
+                                    TRUE ~ "General")) |>
+    # TODO - also check that these should both be classified as disadvantaged
+    mutate(disadvantaged = "Yes")  
   
   # merge PF into the base table
-  oh_base <- oh_base %>%
-    left_join(oh_pf) %>%
-    mutate(estimated_principal_forgiveness = case_when(
+  oh_base <- oh_fundable |>
+    left_join(oh_pf) |>
+    mutate(principal_forgiveness = case_when(
       # fill in gaps where multiple projects or funding_amount discrepancies cause mismatches
       entity == "Walnut Creek Water Company" & estimated_loan_amount == "$3,950,000" ~ "$2,070,000",
       entity == "Rittman" ~ "$2,173,483",
@@ -44,42 +59,50 @@ clean_oh_y1 <- function() {
       entity == "Nelsonville" & loan_type == "Construction" ~ "$2,759,300",
       # replace PF where it attached to two projects
       project == "Village of Mantua Water Treatment Plant Liquid Chlorine" ~ as.character(NA),
-      TRUE ~ estimated_principal_forgiveness
-    ))
-
+      TRUE ~ estimated_principal_forgiveness), 
+      project_type = case_when(grepl("LSL", rate) ~ "Lead", 
+                               TRUE ~ project_type)) 
   
-  # -> (440,12)
-  oh_clean <- oh_base %>%
-    # process numeric columns
-    mutate(population = clean_numeric_string(population),
-           funding_amount = clean_numeric_string(estimated_loan_amount),
-           principal_forgiveness = clean_numeric_string(estimated_principal_forgiveness),
-    ) %>%
-    # process text columns
+  
+  # adding lead ppl: (59, 8)
+  oh_lead_ppl <- fread("year1/OH/data/oh-lslr.csv",
+                       colClasses = "character", na.strings = "") |>
+    clean_names() |> 
+    mutate(project_type = "Lead")
+  
+  # adding EC ppl:(9, 7)
+  oh_pfas_ppl <- fread("year1/OH/data/oh-ppl-hab-pfas.csv",
+                       colClasses = "character", na.strings = "") |>
+    clean_names() |>
+    mutate(project_type = "Emerging Contaminants")
+  
+  # combining everything together: 
+  oh_clean <- bind_rows(oh_base, oh_lead_ppl, oh_pfas_ppl) |>
     mutate(borrower = str_squish(entity),
-           project_description = str_squish(project),
-           pwsid = str_squish(pwsid),
-           community_served = str_squish(county),
-           state = "Ohio",
-           state_fiscal_year = "2023",
-           expecting_funding = "Yes",
-           project_type = case_when(
-             grepl("HAB", rate) | grepl("PFAS", rate)  ~ "Emerging Contaminants",
-             grepl("Lead", project_description) | grepl("LSL", project_description) | grepl("LSL", rate) ~ "Lead",
-             TRUE ~ "General"),
-           disadvantaged = case_when(
-             grepl("DIS", rate) ~ "Yes",
-             TRUE ~ "No"),
+           pwsid = case_when(is.na(pwsid) ~ "No Information", 
+                             TRUE ~ pwsid),
            project_id = as.character(NA),
            project_name = as.character(NA),
+           principal_forgiveness = clean_numeric_string(principal_forgiveness),
+           funding_amount = clean_numeric_string(estimated_loan_amount),
+           project_description = str_squish(project),
+           community_served = str_squish(county),  # TODO <- check this 
            project_cost = as.character(NA),
-           requested_amount = as.character(NA),
-           project_rank = as.character(NA),
-           project_score = as.character(NA),
-    ) %>%
-    select(community_served, borrower, pwsid, project_id, project_name, project_type, project_cost,
-           requested_amount, funding_amount, principal_forgiveness, population, project_description,
-           disadvantaged, project_rank, project_score, expecting_funding, state, state_fiscal_year)
+           requested_amount = clean_numeric_string(estimated_loan_amount), 
+           project_description = str_squish(project),
+           population = clean_numeric_string(population),
+           disadvantaged = case_when(is.na(disadvantaged) ~ "No", 
+                                     TRUE ~ disadvantaged), 
+           project_rank = as.character(NA), 
+           project_score = case_when(is.na(project_score) ~ "No Information", 
+                                     TRUE ~ project_score),
+           expecting_funding = case_when(is.na(expecting_funding) ~ "No", 
+                                     TRUE ~ expecting_funding), 
+           state = "Ohio", 
+           state_fiscal_year = "2023") |>
+    select(community_served, borrower, pwsid, project_id, project_name, project_type, project_cost, requested_amount,
+           funding_amount, principal_forgiveness, population, project_description, disadvantaged, project_rank,
+           project_score, expecting_funding, state, state_fiscal_year)
   
   run_tests(oh_clean)
   rm(list=setdiff(ls(), "oh_clean"))

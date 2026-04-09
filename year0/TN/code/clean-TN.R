39938f32c634d32f3457d2f988b6db82d7b5ea44
mariaEPIC <maria@policyinnovation.org>
2026-04-09 12:27:05 -0400

    TN lead audit

--- year0/TN/code/clean-TN.R
+++ year0/TN/code/clean-TN.R
@@ -1,47 +1,48 @@
 clean_tn_y0 <- function() {
   
-  # there were a few new additions to the EC string on June 12th 2025 to include
-  # various new strings 
-  ec_str_tn <- "cyanotoxin|dioxane|emerging contaminant|lithium|manganese|Mn|Perfluoro-n-pentanoic acid|PFPeA|PFAS|PFOA|PFOS|trihalomethane|THM|Unregulated Contaminant Monitoring Rule|DBP|disinfection byproduct|HAA5|haloacetic acid"
-  
   # (73, 17) - one of these is a total column that needs to be removed
-  tn_ppl <- read.csv("year0/TN/data/tn-srf-dw-fy2021-priority-ranking-list.csv") %>%
-    clean_names()
+  tn_ppl <- data.table::fread("year0/TN/data/tn-srf-dw-fy2021-priority-ranking-list.csv",
+                  colClasses = "character", na.strings = "") |>
+    janitor::clean_names() |>
+    dplyr::mutate(
+      list = "SFY22 Base PPL",
+      pwsid = pwsid_number
+    )
   
-  tn_clean <- tn_ppl %>%
-    filter(local_government != "") %>%
-    mutate(
-      community_served = str_squish(county),
-      borrower = str_squish(local_government),
-      borrower = str_replace_all(borrower, "\\*", ""),
-      borrower = str_replace_all(borrower, "\\+", ""),
-      pwsid = str_squish(pwsid),
+  tn_clean <- tn_ppl |>
+    dplyr::filter(local_government != "") |>
+    dplyr::mutate(
+      community_served = stringr::str_squish(county),
+      borrower = stringr::str_squish(local_government),
+      borrower = stringr::str_replace_all(borrower, "\\*", ""),
+      borrower = stringr::str_replace_all(borrower, "\\+", ""),
+      pwsid = stringr::str_squish(pwsid),
       project_cost = clean_numeric_string(total_project_amount),
-      project_description = str_squish(project_description),
+      project_description = stringr::str_squish(project_description),
       population = clean_numeric_string(pop),
-      project_rank = str_squish(rank_order),
-      project_score = str_squish(priority_points),
-      expecting_funding = case_when(rank_order <= 30 ~ "Yes", 
+      project_rank = stringr::str_squish(rank_order),
+      project_score = stringr::str_squish(priority_points),
+      expecting_funding = dplyr::case_when(rank_order <= 30 ~ "Yes", 
                                     TRUE ~ "No"),
       project_id = as.character(NA),
       project_name = as.character(NA),
-      project_description = str_squish(project_description), 
-           project_type = case_when(
-             grepl(lead_str, project_description, ignore.case=TRUE) ~ "Lead",
+      project_description = stringr::str_squish(project_description), 
+           project_type = dplyr::case_when(
+             grepl("lsl|lead", project_description, ignore.case=TRUE) ~ "Lead",
              grepl(ec_str, project_description, ignore.case=TRUE) ~ "Emerging Contaminants",
              TRUE ~ "General"), 
       requested_amount = as.character(NA),
       funding_amount = as.character(NA),
       principal_forgiveness = as.character(NA),
-      disadvantaged = case_when(atpi > 50 ~ "No", 
+      disadvantaged = dplyr::case_when(atpi > 50 ~ "No", 
                                 TRUE ~ "Yes"),
       state = "Tennessee",
       state_fiscal_year = "2022",
-    ) %>%
-    select(community_served, borrower, pwsid, project_id, project_name, project_type,
+    ) |>
+    dplyr::select(community_served, borrower, pwsid, project_id, project_name, project_type,
            project_cost, requested_amount, funding_amount, principal_forgiveness,
            project_description, population, disadvantaged, project_rank, project_score,
-           expecting_funding, state, state_fiscal_year)
+           expecting_funding, state, state_fiscal_year, list)
   
   ####### SANITY CHECKS START #######
   
@@ -49,42 +50,42 @@ clean_tn_y0 <- function() {
   ####### Decision: No project id
   
   # Check for disinfection byproduct in description
-  tn_clean |> dplyr::filter(grepl("disinfection byproduct", project_description))
-  ####### Decision: Disinfection byproduct string
+  #tn_clean |> dplyr::filter(grepl("disinfection byproduct", project_description))
+  ####### Decision: True disinfection byproduct string
   
   # Check for lead subtypes: Both
-  tn_clean |>
-    dplyr::filter(project_type=="Lead") |>
-    dplyr::mutate(
-      lead_type = dplyr::case_when(
-        stringr::str_detect(tolower(project_description), lsli_str) & stringr::str_detect(tolower(project_description), lslr_str) ~ "both",
-        stringr::str_detect(tolower(project_description), lsli_str) ~ "lsli",
-        stringr::str_detect(tolower(project_description), lslr_str) ~ "lslr",
-        # catch weird exceptions where replacement/inventory doesn't appear next to LSL but should still be marked lslr/i
-        stringr::str_detect(tolower(project_description), "replacement") & stringr::str_detect(tolower(project_description), lead_str) ~ "lslr",
-        stringr::str_detect(tolower(project_description), "inventory") & stringr::str_detect(tolower(project_description), lead_str) ~ "lsli",
-        TRUE ~ "unknown"
-      )
-    ) |>
-    dplyr::filter(lead_type == "both")
+  # tn_clean |>
+  #   dplyr::filter(project_type=="Lead") |>
+  #   dplyr::mutate(
+  #     lead_type = dplyr::case_when(
+  #       stringr::str_detect(tolower(project_description), lsli_str) & stringr::str_detect(tolower(project_description), lslr_str) ~ "both",
+  #       stringr::str_detect(tolower(project_description), lsli_str) ~ "lsli",
+  #       stringr::str_detect(tolower(project_description), lslr_str) ~ "lslr",
+  #       # catch weird exceptions where replacement/inventory doesn't appear next to LSL but should still be marked lslr/i
+  #       stringr::str_detect(tolower(project_description), "replacement") & stringr::str_detect(tolower(project_description), lead_str) ~ "lslr",
+  #       stringr::str_detect(tolower(project_description), "inventory") & stringr::str_detect(tolower(project_description), lead_str) ~ "lsli",
+  #       TRUE ~ "unknown"
+  #     )
+  #   ) |>
+  #   dplyr::filter(lead_type == "both")
 
   ####### Decision: No lead projects classified as both
   
   # Check for lead subtypes: Unknown
-  tn_clean |>
-    dplyr::filter(project_type=="Lead") |>
-    dplyr::mutate(
-      lead_type = dplyr::case_when(
-        stringr::str_detect(tolower(project_description), lsli_str) & stringr::str_detect(tolower(project_description), lslr_str) ~ "both",
-        stringr::str_detect(tolower(project_description), lsli_str) ~ "lsli",
-        stringr::str_detect(tolower(project_description), lslr_str) ~ "lslr",
-        # catch weird exceptions where replacement/inventory doesn't appear next to LSL but should still be marked lslr/i
-        stringr::str_detect(tolower(project_description), "replacement") & stringr::str_detect(tolower(project_description), lead_str) ~ "lslr",
-        stringr::str_detect(tolower(project_description), "inventory") & stringr::str_detect(tolower(project_description), lead_str) ~ "lsli",
-        TRUE ~ "unknown"
-      )
-    ) |>
-    dplyr::filter(lead_type == "unknown")
+  # tn_clean |>
+  #   dplyr::filter(project_type=="Lead") |>
+  #   dplyr::mutate(
+  #     lead_type = dplyr::case_when(
+  #       stringr::str_detect(tolower(project_description), lsli_str) & stringr::str_detect(tolower(project_description), lslr_str) ~ "both",
+  #       stringr::str_detect(tolower(project_description), lsli_str) ~ "lsli",
+  #       stringr::str_detect(tolower(project_description), lslr_str) ~ "lslr",
+  #       # catch weird exceptions where replacement/inventory doesn't appear next to LSL but should still be marked lslr/i
+  #       stringr::str_detect(tolower(project_description), "replacement") & stringr::str_detect(tolower(project_description), lead_str) ~ "lslr",
+  #       stringr::str_detect(tolower(project_description), "inventory") & stringr::str_detect(tolower(project_description), lead_str) ~ "lsli",
+  #       TRUE ~ "unknown"
+  #     )
+  #   ) |>
+  #   dplyr::filter(lead_type == "unknown")
 
   ####### Decision: No lead projects classified as unknown
 
--- year1/TN/code/clean-TN.R
+++ year1/TN/code/clean-TN.R
@@ -1,49 +1,46 @@
 clean_tn_y1 <- function() {
   
-  # there were a few new additions to the EC string on June 12th 2025 to include
-  # various new strings 
-  ec_str_tn <- "cyanotoxin|dioxane|emerging contaminant|lithium|manganese|Mn|Perfluoro-n-pentanoic acid|PFPeA|PFAS|PFOA|PFOS|trihalomethane|THM|Unregulated Contaminant Monitoring Rule|DBP|disinfection byproduct|HAA5|haloacetic acid"
-  
   # (143, 19) - this file has been updated with the correct one on 
   # March 19th 2025
-  tn_ppl <- fread("year1/TN/data/sfy_23_basegen_supp_ppl.csv",
-                  colClasses = "character", na.strings = "") %>%
-    clean_names() 
+  tn_ppl <- data.table::fread("year1/TN/data/sfy_23_basegen_supp_ppl.csv",
+                  colClasses = "character", na.strings = "") |>
+    janitor::clean_names() |>
+    dplyr::mutate(list = "SFY23 Base Gen") 
   
-  tn_clean <- tn_ppl %>%
+  tn_clean <- tn_ppl |>
     # # process numeric columns
-    mutate(project_cost = clean_numeric_string(total_project_amount),
-           population = clean_numeric_string(pop_served)) %>%
+    dplyr::mutate(project_cost = clean_numeric_string(total_project_amount),
+           population = clean_numeric_string(pop_served)) |>
     # process text columns 
-    mutate(community_served = str_squish(county),
-           borrower = str_squish(local_government),
-           pwsid = str_squish(pwsid_number),
+    dplyr::mutate(community_served = str_squish(county),
+           borrower = stringr::str_squish(local_government),
+           pwsid = stringr::str_squish(pwsid_number),
            project_id = as.character(NA),
            project_name = as.character(NA), 
-           project_description = str_squish(project_description), 
+           project_description = stringr::str_squish(project_description), 
            project_type = case_when(
-             grepl(lead_str, project_description, ignore.case=TRUE) ~ "Lead",
+             grepl("lsl|lead", project_description, ignore.case=TRUE) ~ "Lead",
              grepl(ec_str, project_description, ignore.case=TRUE) ~ "Emerging Contaminants",
              TRUE ~ "General"), 
            requested_amount = as.character(NA), 
            funding_amount = as.character(NA),
            principal_forgiveness = as.character(NA), 
-           project_description = str_squish(project_description),
-           disadvantaged = case_when(
+           project_description = stringr::str_squish(project_description),
+           disadvantaged = dplyr::case_when(
              grepl("\\*", local_government) ~ "Yes",
              TRUE ~ "No"),
            # with dac define, remove extra characters referencing table footnotes
-           borrower = str_replace_all(borrower, "\\*", ""),
-           borrower = str_replace_all(borrower, "\\+", ""),
-           project_rank = str_squish(rank_order),
-           project_score = str_squish(priority_points),
+           borrower = stringr::str_replace_all(borrower, "\\*", ""),
+           borrower = stringr::str_replace_all(borrower, "\\+", ""),
+           project_rank = stringr::str_squish(rank_order),
+           project_score = stringr::str_squish(priority_points),
            expecting_funding = ifelse(row_number() < 108, "Yes", "No"),
            state = "Tennessee",
            state_fiscal_year = "2023"
-    ) %>%
-    select(community_served, borrower, pwsid, project_id, project_name, project_type, project_cost,
+    ) |>
+    dplyr::select(community_served, borrower, pwsid, project_id, project_name, project_type, project_cost,
            requested_amount, funding_amount, principal_forgiveness, population, project_description,
-           disadvantaged, project_rank, project_score, expecting_funding, state, state_fiscal_year)
+           disadvantaged, project_rank, project_score, expecting_funding, state, state_fiscal_year, list)
   
   ####### SANITY CHECKS START #######
   
@@ -51,42 +48,42 @@ clean_tn_y1 <- function() {
   ####### Decision: No project id
   
   # Check for disinfection byproduct in description
-  tn_clean |> dplyr::filter(grepl("disinfection byproduct", project_description))
+  # tn_clean |> dplyr::filter(grepl("disinfection byproduct", project_description))
   ####### Decision: Disinfection byproduct string
   
   # Check for lead subtypes: Both
-  tn_clean |>
-    dplyr::filter(project_type=="Lead") |>
-    dplyr::mutate(
-      lead_type = dplyr::case_when(
-        stringr::str_detect(tolower(project_description), lsli_str) & stringr::str_detect(tolower(project_description), lslr_str) ~ "both",
-        stringr::str_detect(tolower(project_description), lsli_str) ~ "lsli",
-        stringr::str_detect(tolower(project_description), lslr_str) ~ "lslr",
-        # catch weird exceptions where replacement/inventory doesn't appear next to LSL but should still be marked lslr/i
-        stringr::str_detect(tolower(project_description), "replacement") & stringr::str_detect(tolower(project_description), lead_str) ~ "lslr",
-        stringr::str_detect(tolower(project_description), "inventory") & stringr::str_detect(tolower(project_description), lead_str) ~ "lsli",
-        TRUE ~ "unknown"
-      )
-    ) |>
-    dplyr::filter(lead_type == "both")
+  # tn_clean |>
+  #   dplyr::filter(project_type=="Lead") |>
+  #   dplyr::mutate(
+  #     lead_type = dplyr::case_when(
+  #       stringr::str_detect(tolower(project_description), lsli_str) & stringr::str_detect(tolower(project_description), lslr_str) ~ "both",
+  #       stringr::str_detect(tolower(project_description), lsli_str) ~ "lsli",
+  #       stringr::str_detect(tolower(project_description), lslr_str) ~ "lslr",
+  #       # catch weird exceptions where replacement/inventory doesn't appear next to LSL but should still be marked lslr/i
+  #       stringr::str_detect(tolower(project_description), "replacement") & stringr::str_detect(tolower(project_description), lead_str) ~ "lslr",
+  #       stringr::str_detect(tolower(project_description), "inventory") & stringr::str_detect(tolower(project_description), lead_str) ~ "lsli",
+  #       TRUE ~ "unknown"
+  #     )
+  #   ) |>
+  #   dplyr::filter(lead_type == "both")
 
   ####### Decision: No lead projects classified as both
   
   # Check for lead subtypes: Unknown
-  tn_clean |>
-    dplyr::filter(project_type=="Lead") |>
-    dplyr::mutate(
-      lead_type = dplyr::case_when(
-        stringr::str_detect(tolower(project_description), lsli_str) & stringr::str_detect(tolower(project_description), lslr_str) ~ "both",
-        stringr::str_detect(tolower(project_description), lsli_str) ~ "lsli",
-        stringr::str_detect(tolower(project_description), lslr_str) ~ "lslr",
-        # catch weird exceptions where replacement/inventory doesn't appear next to LSL but should still be marked lslr/i
-        stringr::str_detect(tolower(project_description), "replacement") & stringr::str_detect(tolower(project_description), lead_str) ~ "lslr",
-        stringr::str_detect(tolower(project_description), "inventory") & stringr::str_detect(tolower(project_description), lead_str) ~ "lsli",
-        TRUE ~ "unknown"
-      )
-    ) |>
-    dplyr::filter(lead_type == "unknown")
+  # tn_clean |>
+  #   dplyr::filter(project_type=="Lead") |>
+  #   dplyr::mutate(
+  #     lead_type = dplyr::case_when(
+  #       stringr::str_detect(tolower(project_description), lsli_str) & stringr::str_detect(tolower(project_description), lslr_str) ~ "both",
+  #       stringr::str_detect(tolower(project_description), lsli_str) ~ "lsli",
+  #       stringr::str_detect(tolower(project_description), lslr_str) ~ "lslr",
+  #       # catch weird exceptions where replacement/inventory doesn't appear next to LSL but should still be marked lslr/i
+  #       stringr::str_detect(tolower(project_description), "replacement") & stringr::str_detect(tolower(project_description), lead_str) ~ "lslr",
+  #       stringr::str_detect(tolower(project_description), "inventory") & stringr::str_detect(tolower(project_description), lead_str) ~ "lsli",
+  #       TRUE ~ "unknown"
+  #     )
+  #   ) |>
+  #   dplyr::filter(lead_type == "unknown")
 
   ####### Decision: No lead projects classified as unknown
 
